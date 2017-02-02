a5569130bb17ce2cf1adf6112a59bd727589d011
Josh Soref <jsoref@users.noreply.github.com>
2017-02-02 05:01:09 +0000

    spelling: you

--- contrib/reverse-dns/reverse_replace.sh
+++ contrib/reverse-dns/reverse_replace.sh
@@ -5,7 +5,7 @@
 # Parses stdin for IP4 addresses and replaces them 
 # with names retrieved by parsing the dnsmasq log.
 # This currently only gives CNAMEs. But these 
-# usually tell ou more than the ones from reverse 
+# usually tell you more than the ones from reverse 
 # lookups. 
 #
 # This has been tested on debian and asuswrt. Please
